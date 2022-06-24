e8e8404c4da56eaade0e43c4b72256d8784a2955
Tejprakash Upadhyay <93568571+TejPrakash18@users.noreply.github.com>
2022-06-24 13:01:00 +0530

    init

---
+++ DataTypes/DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+    public static void main(String[] args) {
+        int num = 5; // store all integer number
+        float f = 5.99f;  // Store all Float number
+        String str = "Hello"; //Store String & Sentance 
+        char ch = 'A';  // store a character
+        boolean bool = true;  // Return True or false 
+
+        System.out.println(num);
+        System.out.println(f);
+        System.out.println(str);
+        System.out.println(bool);
+        System.out.println(ch);
+    }
+    
+}
