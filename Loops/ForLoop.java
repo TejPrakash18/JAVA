2d910652cf5199c7927372142187e888a50ec521
Tejprakash Upadhyay <93568571+TejPrakash18@users.noreply.github.com>
2022-06-24 13:39:26 +0530

    init

---
+++ Loops/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        // print num 1 to 10
+        System.out.println("Print 1 to 10 Number");
+        for(int i=1 ; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
---
+++ Loops/forEachLoop.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+
+public class forEachLoop {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40};  
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+    
+    
+}
